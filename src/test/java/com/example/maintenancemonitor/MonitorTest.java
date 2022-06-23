45b3133c57692975d769f793b5c2935834e648a4
Maria Puthenveetil <wi21b092@technikum-wien.at>
2022-06-23 12:55:46 +0200

    this commit will add the tests for /status function

--- src/test/java/com/example/maintenancemonitor/MonitorTest.java
+++ src/test/java/com/example/maintenancemonitor/MonitorTest.java
@@ -14,11 +14,33 @@ class MonitorTest {
 
         assertEquals(50, monitor.getPercent());
     }
-
     @Test
     void changeTest() {
         double a = 86;
 
         assertEquals("Status level changed to 86.0%", monitor.change(a));
+    }
+
+    @Test
+    void difference() {
+        double a = 50;
+
+        assertEquals(49.95, monitor.difference(a));
+    }
+
+    @Test
+    void correctMessageTest() {
+        double a = 99.95;
+
+        assertEquals("Current service level agreement: 99.95%." + "<br>" +
+                "Status level is ok!", monitor.correctMessage(a));
+    }
+
+    @Test
+    void falseMessageTest() {
+        double a = 99.94;
+
+        assertEquals("Current service level agreement: 99.94%." + "<br>" +
+                "Status level is not ok as its 0.010000000000005116% too low!", monitor.falseMessage(a));
     }
 }
